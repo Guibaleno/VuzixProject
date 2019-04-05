3a9679c3e3df8217d098a5c1708282c74ae1ddfb
willio44 <42750120+willio44@users.noreply.github.com>
2019-04-05 09:32:12 -0400

    Premier commit licensePlate 2.0

--- speech_recognition/app/src/main/java/com/vuzix/sample/m300_speech_recognition/Connections/ConnectionAPILicensePlate.java
+++ speech_recognition/app/src/main/java/com/vuzix/sample/m300_speech_recognition/Connections/ConnectionAPILicensePlate.java
@@ -1,4 +1,88 @@
 package com.vuzix.sample.m300_speech_recognition.Connections;
 
-public class ConnectionAPILicensePlate {
+import com.vuzix.sample.m300_speech_recognition.HeaderInfo;
+import com.vuzix.sample.m300_speech_recognition.LicensePlate;
+import com.vuzix.sample.m300_speech_recognition.Login;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.HashMap;
+
+public class ConnectionAPILicensePlate extends ConnectionAPI {
+    LicensePlate mLicensePlate;
+
+    public ConnectionAPILicensePlate(LicensePlate newLicensePlate, String apiAdress) {
+        super();
+        mLicensePlate = newLicensePlate;
+        APIAdress = apiAdress;
+
+    }
+
+    @Override
+    protected void onPostExecute(String response) {
+        if (checknetwork(mLicensePlate)) {
+            if (response != null) {
+
+                try {
+                    JSONObject jsonObject = new JSONObject(response);
+                    if (jsonObject.has("token")) {
+                        HeaderInfo.setToken(jsonObject.getString("token"));
+                        mLicensePlate.MoveToWarehouse();
+                    } else if (jsonObject.has("English")) {
+                        mLicensePlate.Toast(jsonObject.getString("English"));
+                    }
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                    mLicensePlate.HideProgress();
+                }
+            }
+            mLicensePlate.HideProgress();
+        }
+
+    }
+
+    @Override
+    public String ManageConnection() {
+        StringBuffer jsonString = new StringBuffer();
+        try {
+
+
+            URL url = new URL(APIAdress);
+
+            connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("POST");
+            connection.setDoInput(true);
+            connection.setInstanceFollowRedirects(false);
+            connection.setRequestProperty("Content-Type", "application/json; charset=UTF-8");
+            connection.connect();
+
+            HashMap<String, String> params = new HashMap<String, String>();
+            params.put("user","guillaume");
+            params.put("password","guillaume");
+            params.put("companyName",mLicensePlate.getIntent().getStringExtra("lisadbName"));
+
+            JSONObject obj = new JSONObject(params);
+            String payload = obj.toString();
+            OutputStreamWriter os = new OutputStreamWriter(connection.getOutputStream(), "UTF-8");
+            os.write(payload);
+            os.close();
+            BufferedReader br = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+            String line;
+
+            while ((line = br.readLine()) != null)
+            {
+                jsonString.append(line);
+            }
+            br.close();
+            connection.disconnect();
+        }
+        catch (Exception e){}
+        return jsonString.toString();
+    }
 }
--- speech_recognition/app/src/main/java/com/vuzix/sample/m300_speech_recognition/VoiceCmdLicensePlate.java
+++ speech_recognition/app/src/main/java/com/vuzix/sample/m300_speech_recognition/VoiceCmdLicensePlate.java
@@ -1,4 +1,197 @@
 package com.vuzix.sample.m300_speech_recognition;
 
-public class VoiceCmdLicensePlate {
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.vuzix.sdk.speechrecognitionservice.VuzixSpeechClient;
+import static android.view.KeyEvent.KEYCODE_A;
+import static android.view.KeyEvent.KEYCODE_AT;
+import static android.view.KeyEvent.KEYCODE_B;
+import static android.view.KeyEvent.KEYCODE_C;
+import static android.view.KeyEvent.KEYCODE_CAPS_LOCK;
+import static android.view.KeyEvent.KEYCODE_D;
+import static android.view.KeyEvent.KEYCODE_DEL;
+import static android.view.KeyEvent.KEYCODE_E;
+import static android.view.KeyEvent.KEYCODE_ENTER;
+import static android.view.KeyEvent.KEYCODE_F;
+import static android.view.KeyEvent.KEYCODE_G;
+import static android.view.KeyEvent.KEYCODE_H;
+import static android.view.KeyEvent.KEYCODE_I;
+import static android.view.KeyEvent.KEYCODE_J;
+import static android.view.KeyEvent.KEYCODE_K;
+import static android.view.KeyEvent.KEYCODE_L;
+import static android.view.KeyEvent.KEYCODE_M;
+import static android.view.KeyEvent.KEYCODE_N;
+import static android.view.KeyEvent.KEYCODE_O;
+import static android.view.KeyEvent.KEYCODE_P;
+import static android.view.KeyEvent.KEYCODE_PERIOD;
+import static android.view.KeyEvent.KEYCODE_Q;
+import static android.view.KeyEvent.KEYCODE_R;
+import static android.view.KeyEvent.KEYCODE_S;
+import static android.view.KeyEvent.KEYCODE_SHIFT_LEFT;
+import static android.view.KeyEvent.KEYCODE_SPACE;
+import static android.view.KeyEvent.KEYCODE_T;
+import static android.view.KeyEvent.KEYCODE_U;
+import static android.view.KeyEvent.KEYCODE_V;
+import static android.view.KeyEvent.KEYCODE_W;
+import static android.view.KeyEvent.KEYCODE_X;
+import static android.view.KeyEvent.KEYCODE_Y;
+import static android.view.KeyEvent.KEYCODE_Z;
+import static android.view.KeyEvent.KEYCODE_MINUS;
+
+
+public class VoiceCmdLicensePlate extends VoiceCmdReceiver {
+    private LicensePlate mLicensePLate;
+    final String MATCH_RETURN_TO_ORDERS = "ReturnToOrders";
+    public final String MATCH_RELOAD_LICENSEPLATE = "RealoadLicensePlate";
+    public final String MATCH_NEXT_INFOORDERS = "NextInfoOrders";
+    //final String MATCH_BACK     = "back";
+    public VoiceCmdLicensePlate(LicensePlate iActivity)
+    {
+        mLicensePLate = iActivity;
+        mLicensePLate.registerReceiver(this, new IntentFilter(VuzixSpeechClient.ACTION_VOICE_COMMAND));
+        //Log.d(mLogin.LOG_TAG, "Connecting to M300 SDK");
+
+
+        try {
+            // Create a VuzixSpeechClient from the SDK
+            //Log.d(mLogin.LOG_TAG, iActivity.toString());
+            sc = new VuzixSpeechClient(iActivity);
+            // Delete specific phrases. This is useful if there are some that sound similar to yours, but
+            // you want to keep the majority of them intact
+            //sc.deletePhrase("torch on");
+            //sc.deletePhrase("torch on");
+
+            // Delete every phrase in the dictionary
+            sc.deletePhrase("*");
+
+            // Now add any new strings.  If you put a substitution in the second argument, you will be passed that string instead of the full string
+            sc.insertKeycodePhrase("A", KEYCODE_A );
+            sc.insertKeycodePhrase("Bee", KEYCODE_B);
+            sc.insertKeycodePhrase("Cee", KEYCODE_C);
+            sc.insertKeycodePhrase("Dee", KEYCODE_D);
+            sc.insertKeycodePhrase("Ee", KEYCODE_E);
+            sc.insertKeycodePhrase("Eff", KEYCODE_F);
+            sc.insertKeycodePhrase("Gee", KEYCODE_G);
+            sc.insertKeycodePhrase("aich", KEYCODE_H);
+            sc.insertKeycodePhrase("I", KEYCODE_I);
+            sc.insertKeycodePhrase("Jay", KEYCODE_J);
+            sc.insertKeycodePhrase("Kay", KEYCODE_K);
+            sc.insertKeycodePhrase("Ell", KEYCODE_L);
+            sc.insertKeycodePhrase("El", KEYCODE_L);
+            sc.insertKeycodePhrase("Em", KEYCODE_M);
+            sc.insertKeycodePhrase("En", KEYCODE_N);
+            sc.insertKeycodePhrase("O", KEYCODE_O);
+            sc.insertKeycodePhrase("Pee", KEYCODE_P);
+            sc.insertKeycodePhrase("Cue", KEYCODE_Q);
+            sc.insertKeycodePhrase("Ar", KEYCODE_R);
+            sc.insertKeycodePhrase("Ess", KEYCODE_S);
+            sc.insertKeycodePhrase("Tee", KEYCODE_T);
+            sc.insertKeycodePhrase("U", KEYCODE_U);
+            sc.insertKeycodePhrase("Vee", KEYCODE_V);
+            sc.insertKeycodePhrase("Double-u", KEYCODE_W);
+            sc.insertKeycodePhrase("Ex", KEYCODE_X);
+            sc.insertKeycodePhrase("Wy", KEYCODE_Y);
+            sc.insertKeycodePhrase("Zed", KEYCODE_Z);
+            sc.insertKeycodePhrase("Zee", KEYCODE_Z);
+            sc.insertKeycodePhrase("Space", KEYCODE_SPACE);
+            sc.insertKeycodePhrase("shift", KEYCODE_SHIFT_LEFT);
+            sc.insertKeycodePhrase("caps lock", KEYCODE_CAPS_LOCK);
+            sc.insertKeycodePhrase("at sign", KEYCODE_AT);
+            sc.insertKeycodePhrase("period", KEYCODE_PERIOD);
+            sc.insertKeycodePhrase("erase", KEYCODE_DEL);
+            sc.insertKeycodePhrase("enter", KEYCODE_ENTER);
+            sc.insertKeycodePhrase("hyphen", KEYCODE_MINUS);
+
+            // Insert a custom intent.  Note: these are sent with sendBroadcastAsUser() from the service
+            // If you are sending an event to another activity, be sure to test it from the adb shell
+            // using: am broadcast -a "<your intent string>"
+            // This example sends it to ourself, and we are sure we are active and registered for it
+            Intent customToastIntent = new Intent(mLicensePLate.CUSTOM_SDK_INTENT);
+            sc.defineIntent(TOAST_EVENT, customToastIntent );
+            sc.insertIntentPhrase("canned toast", TOAST_EVENT);
+            sc.insertPhrase(MATCH_RETURN, MATCH_RETURN_TO_COMPANIES);
+            sc.insertPhrase(MATCH_NEXT, MATCH_NEXT_INFOORDERS);
+            sc.insertPhrase(MATCH_RELOAD, MATCH_RELOAD_LOGIN);
+            Log.i("", sc.toString());
+
+
+            // See what we've done
+            mLicensePLate
+            // The recognizer may not yet be enabled in Settings. We can enable this directly
+            VuzixSpeechClient.EnableRecognizer(mLogin, true);
+        } catch(NoClassDefFoundError e) {
+            // We get this exception if the SDK stubs against which we compiled cannot be resolved
+            // at runtime. This occurs if the code is not being run on an M300 supporting the voice
+            // SDK
+            Toast.makeText(iActivity, R.string.only_on_m300, Toast.LENGTH_LONG).show();
+            Log.e(mLicensePLate.LOG_TAG, iActivity.getResources().getString(R.string.only_on_m300) );
+            Log.e(mLicensePLate.LOG_TAG, e.getMessage());
+            e.printStackTrace();
+            iActivity.finish();
+        } catch (Exception e) {
+            Log.e(mLicensePLate.LOG_TAG, "Error setting custom vocabulary: " + e.getMessage());
+            e.printStackTrace();
+        }
+    }
+    /**
+     * All custom phrases registered with insertPhrase() are handled here.
+     *
+     * Custom intents may also be directed here, but this example does not demonstrate this.
+     *
+     * Keycodes are never handled via this interface
+     *
+     * @param context Context in which the phrase is handled
+     * @param intent Intent associated with the recognized phrase
+     */
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        // All phrases registered with insertPhrase() match ACTION_VOICE_COMMAND as do
+        // recognizer status updates
+        if (intent.getAction().equals(VuzixSpeechClient.ACTION_VOICE_COMMAND)) {
+            Bundle extras = intent.getExtras();
+            if (extras != null) {
+                // We will determine what type of message this is based upon the extras provided
+                if (extras.containsKey(VuzixSpeechClient.PHRASE_STRING_EXTRA)) {
+                    // If we get a phrase string extra, this was a recognized spoken phrase.
+                    // The extra will contain the text that was recognized, unless a substitution
+                    // was provided.  All phrases in this example have substitutions as it is
+                    // considered best practice
+                    String phrase = intent.getStringExtra(VuzixSpeechClient.PHRASE_STRING_EXTRA);
+
+                    // Determine the specific phrase that was recognized and act accordingly
+                    if (phrase.equals(MATCH_RETURN_TO_ORDERS))
+                    {
+                        mLicensePLate.FinishActivity();
+                    }
+                    else if (phrase.equals(MATCH_NEXT_INFOORDERS))
+                    {
+                        mLicensePLate.ShowProgress();
+                        mLicensePLate.TryToConnect();
+                    }
+                    else if (phrase.equals(MATCH_RELOAD_LICENSEPLATE))
+                    {
+                        mLicensePLate.Reload();
+                    }
+                    else
+                    {
+
+                    }
+                }
+            }
+        }
+    }
+    public void unregister() {
+        try {
+            mLicensePLate.unregisterReceiver(this);
+            Log.i(mLicensePLate.LOG_TAG, "Custom vocab removed");
+            mLicensePLate = null;
+        }catch (Exception e) {
+            Log.e(mLicensePLate.LOG_TAG, "Custom vocab died " + e.getMessage());
+        }
+    }
 }
