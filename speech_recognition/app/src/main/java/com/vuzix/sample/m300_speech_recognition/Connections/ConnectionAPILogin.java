ad0dd9730cde7464d947e7fb86c19aaf3631ffc2
B438P-15\admin <glambertgm@gmail.com>
2019-04-09 17:04:58 -0400

    Erreur de login complétées

--- speech_recognition/app/src/main/java/com/vuzix/sample/m300_speech_recognition/Connections/ConnectionAPILogin.java
+++ speech_recognition/app/src/main/java/com/vuzix/sample/m300_speech_recognition/Connections/ConnectionAPILogin.java
@@ -1,9 +1,11 @@
 package com.vuzix.sample.m300_speech_recognition.Connections;
 
 import android.util.Log;
+import android.widget.TextView;
 
 import com.vuzix.sample.m300_speech_recognition.Login;
 import com.vuzix.sample.m300_speech_recognition.HeaderInfo;
+import com.vuzix.sample.m300_speech_recognition.R;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -34,12 +36,35 @@ public class ConnectionAPILogin extends ConnectionAPI {
                 try {
 
                     JSONObject jsonObject = new JSONObject(response);
-                    if (jsonObject.has("token")) {
-                        HeaderInfo.setToken(jsonObject.getString("token"));
-                        mLogin.MoveToWarehouse();
-                    } else if (jsonObject.has("English")) {
-                        Alert(mLogin,"Connection error",jsonObject.getString("English"), "Dismiss");
-                        mLogin.ClearPasswordTextbox();
+                    if (!jsonObject.has("errors")) {
+                        if (jsonObject.has("token")) {
+                            HeaderInfo.setToken(jsonObject.getString("token"));
+                            mLogin.MoveToWarehouse();
+                        } else if (jsonObject.has("English")) {
+                            Alert(mLogin, "Connection error", jsonObject.getString("English"), "Dismiss");
+                            mLogin.ClearPasswordTextbox();
+                        }
+                    }
+                    else
+                    {
+                        String title = jsonObject.getString("title");
+                        JSONObject errors = jsonObject.getJSONObject("errors");
+                        String errorMessage = "";
+                        if (errors.has("User"))
+                        {
+                            JSONArray user = errors.getJSONArray("User");
+                            errorMessage += user.getString(0);
+                        }
+                        if (errors.has("Password"))
+                        {
+                            if (!errorMessage.equals(""))
+                            {
+                                errorMessage += "\n";
+                            }
+                            JSONArray password = errors.getJSONArray("Password");
+                            errorMessage += password.getString(0);
+                        }
+                        Alert(mLogin, title, errorMessage,"Dismiss");
                     }
                 } catch (JSONException e) {
                     e.printStackTrace();
@@ -67,13 +92,14 @@ public class ConnectionAPILogin extends ConnectionAPI {
             connection.connect();
 
             HashMap<String, String> params = new HashMap<String, String>();
-            params.put("user","guillaume");
-            params.put("password","p");
+            TextView txtUser = (TextView) mLogin.findViewById(R.id.txtUsername);
+            TextView txtPassword = (TextView) mLogin.findViewById(R.id.txtPassword);
+            params.put("user",txtUser.getText().toString());
+            params.put("password",txtPassword.getText().toString());
             params.put("companyName",mLogin.getIntent().getStringExtra("lisadbName"));
 
             JSONObject obj = new JSONObject(params);
             String payload = obj.toString();
-            Log.d("eee",payload);
             OutputStreamWriter os = new OutputStreamWriter(connection.getOutputStream(), "UTF-8");
             os.write(payload);
             os.close();
