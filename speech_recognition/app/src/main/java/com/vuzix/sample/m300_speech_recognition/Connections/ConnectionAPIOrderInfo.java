422185d29bdc552d83ff2821d89a8c3db7f9947e
willio44 <42750120+willio44@users.noreply.github.com>
2019-04-12 16:09:16 -0400

    commit order info 2

--- speech_recognition/app/src/main/java/com/vuzix/sample/m300_speech_recognition/Connections/ConnectionAPIOrderInfo.java
+++ speech_recognition/app/src/main/java/com/vuzix/sample/m300_speech_recognition/Connections/ConnectionAPIOrderInfo.java
@@ -1,4 +1,106 @@
 package com.vuzix.sample.m300_speech_recognition.Connections;
 
-public class ConnectionAPIOrderInfo {
+import com.vuzix.sample.m300_speech_recognition.HeaderInfo;
+import com.vuzix.sample.m300_speech_recognition.OrderInfo;
+import com.vuzix.sample.m300_speech_recognition.Warehouses;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import javax.net.ssl.HostnameVerifier;
+import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.SSLSession;
+
+public class ConnectionAPIOrderInfo extends ConnectionAPI {
+    OrderInfo mOrderInfo;
+
+    public ConnectionAPIOrderInfo(OrderInfo mNewOrderInfo, String apiAdress)
+    {
+        if (checknetwork(mNewOrderInfo))
+        {
+            mOrderInfo = mNewOrderInfo;
+            APIAdress = apiAdress;
+        }
+    }
+
+    @Override
+    protected void onPostExecute(String response) {
+
+
+        if (response != null) {
+
+            JSONArray jsonRoot = null;
+            try {
+                jsonRoot = new JSONArray(response);
+
+                for (int cptObjects = 0; cptObjects < jsonRoot.length(); cptObjects++) {
+                    JSONObject object = jsonRoot.getJSONObject(cptObjects);
+                    String idWareHouse = object.getString("productName");
+                    mOrderInfo.SetOrderInfoText(idWareHouse);
+                }
+
+
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    @Override
+    public String ManageConnection() {
+        try{
+            HostnameVerifier HostVerification = new HostnameVerifier() {
+                @Override
+                public boolean verify(String s, SSLSession sslSession) {
+                    return true;
+                }
+            };
+            HttpsURLConnection.setDefaultHostnameVerifier(HostVerification);
+            URL url = new URL(APIAdress);
+
+            connection = (HttpURLConnection)url.openConnection();
+            connection.setRequestProperty("Content-Type", "application/json; charset=UTF-8");
+
+
+            connection.setDoInput(true);
+            InputStream in = new BufferedInputStream(connection.getInputStream());
+            connection.connect();
+
+            InputStream inputStream = connection.getInputStream();
+
+            StringBuffer buffer = new StringBuffer();
+
+            if (inputStream == null){
+
+                return null;
+            }
+            reader = new BufferedReader(new InputStreamReader(inputStream));
+            String line;
+            while ((line = reader.readLine()) != null){
+                buffer.append(line + "\n");
+            }
+            if (buffer.length() == 0){
+                return  null;
+            }
+            return buffer.toString();
+        }
+        catch (MalformedURLException e){
+            e.printStackTrace();
+        }
+        catch ( IOException e){e.printStackTrace();} catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
 }
+
--- speech_recognition/app/src/main/java/com/vuzix/sample/m300_speech_recognition/OrderInfo.java
+++ speech_recognition/app/src/main/java/com/vuzix/sample/m300_speech_recognition/OrderInfo.java
@@ -1,13 +1,144 @@
 package com.vuzix.sample.m300_speech_recognition;
 
+import android.app.Activity;
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.vuzix.sample.m300_speech_recognition.Connections.ConnectionAPI;
+import com.vuzix.sample.m300_speech_recognition.Connections.ConnectionAPILicensePlate;
+import com.vuzix.sample.m300_speech_recognition.Barcode.MainBarcode;
+import com.vuzix.sample.m300_speech_recognition.Connections.ConnectionAPIOrderInfo;
+import com.vuzix.sample.m300_speech_recognition.Connections.ConnectionAPIOrders;
+import com.vuzix.sdk.barcode.ScanResult;
+import com.vuzix.sdk.barcode.ScannerIntent;
+
 
 public class OrderInfo extends AppCompatActivity {
+    public final String LOG_TAG = "VoiceSample";
+    public String APIAdress;
+    public final String CUSTOM_SDK_INTENT = "com.vuzix.sample.m300_voicecontrolwithsdk.CustomIntent";
+    private static final int REQUEST_CODE_SCAN = 60000;
 
+
+    TextView txtOrderNo;
+    TextView txtProductCode;
+    TextView txtDescription;
+    TextView txtLicensePlate;
+    TextView txtParentLocs;
+    TextView txtBin;
+
+    EditText txtBinNumber;
+
+    VoiceCmdReceiverScanBarcode mVoiceCmdReceiverScanBarcode;
+    ConnectionAPIOrderInfo connectionOrderInfo;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_order_info);
+        setContentView(R.layout.activity_license_plate);
+
+        txtOrderNo = (TextView) findViewById(R.id.textView_orderNo);
+        txtProductCode = (TextView) findViewById(R.id.textView_ProductCode);
+        txtDescription = (TextView) findViewById(R.id.textView_descr);
+        txtLicensePlate = (TextView) findViewById(R.id.textView_licensePlate);
+        txtParentLocs = (TextView) findViewById(R.id.textView_ParentDoc);
+        txtBin = (TextView) findViewById(R.id.textView_BIN);
+        txtBinNumber = (EditText) findViewById(R.id.editText2);
+
+        //APIAdress = getAPIAdress();
+
+        SetOrderInfoText(txtDescription.toString());
+        WritePreviousData();
+        getOrderInfo();
+        // = new VoiceCmdReceiverScanBarcode(this);
     }
+
+    public void SetOrderInfoText(String descr)
+    {
+        String zoneName = getIntent().getStringExtra("zoneName");
+        String orderNo = getIntent().getStringExtra("idOrder");
+        txtOrderNo.setText(getString(R.string.txtSelectAnOrder)+ " " + orderNo);
+        txtDescription.setText(descr);
+        txtLicensePlate.requestFocus();
+    }
+
+    private void getOrderInfo(){
+
+        try{
+
+            connectionOrderInfo.execute();
+
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+
+    //This function will write the same License plate after a reload
+    void WritePreviousData()
+    {
+        if (getIntent().getStringExtra("licensePlateNo") != null)
+        {
+            txtLicensePlate.setText(getIntent().getStringExtra("licensePlateNo"));
+        }
+    }
+
+    void FinishActivity()
+    {
+        mVoiceCmdReceiverScanBarcode.unregister();
+        mVoiceCmdReceiverScanBarcode = null;
+        finish();
+    }
+
+   //void CreateLicencePlate()
+   //{
+   //    Toast.makeText(getApplicationContext(), "Next", Toast.LENGTH_SHORT).show();
+   //    connection = new ConnectionAPILicensePlate(this, APIAdress);
+   //    connection.execute();
+
+   //}
+
+   //public void MoveToScanner() {
+   //    Intent scannerIntent = new Intent(ScannerIntent.ACTION);
+   //    try {
+   //        // The Vuzix M300 has a built-in Barcode Scanner app that is registered for this intent.
+   //        startActivityForResult(scannerIntent, REQUEST_CODE_SCAN);
+   //    } catch (ActivityNotFoundException activityNotFound) {
+   //        Toast.makeText(this, R.string.only_on_m300, Toast.LENGTH_LONG).show();
+   //    }
+   //}
+
+
+
+    public void Toast(String texte)
+    {
+        Toast.makeText(getApplicationContext(), texte, Toast.LENGTH_SHORT).show();
+    }
+
+
+   //public void Reload()
+   //{
+   //    Intent intent = new Intent(this, LicensePlate.class);
+   //    intent.putExtra("zoneName",txtZone.getText().toString());
+   //    intent.putExtra("idZone",getIntent().getStringExtra("idZone"));
+   //    intent.putExtra("idOrder",txtOrder.getText().toString());
+   //    intent.putExtra("idwareHouse", getIntent().getStringExtra("idwareHouse"));
+   //    intent.putExtra("licensePlateNo",txtLicensePlate.getText().toString());
+   //    startActivity(intent);
+   //    finish();
+   //}
+
+    //public String getAPIAdress()
+    //{
+//
+    //    String orderID = getIntent().getStringExtra("idOrder");
+    //    return  "https://216.226.53.29/V5/API/Products%28" + orderID
+    //            + "%29?";
+    //}
+//
 }
